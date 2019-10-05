b0747bc999a5e472a28a5ac682c39551d8834d89
RJYogiChowdary <rjyogichowdary@gmail.com>
2019-10-05 16:29:20 +0530

    first java program

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AddAnyTwoNumbers 
+{
+	public static void main(String[] args) 
+	{
+		int firstNumber,secondNumber;
+		
+		Scanner scr = new Scanner(System.in);
+		
+		System.out.println("Enter Fisrt Number value: ");
+		firstNumber = scr.nextInt();
+
+		System.out.println("Enter Second Number value: ");
+		secondNumber = scr.nextInt();
+		
+		System.out.println("Addition of any two numbers: "+(firstNumber+secondNumber));
+	}
+}
